349cb732bf77c0f6e54cf953e236e99a3d97225f
Ronald Li <ronaldbrooklynli@gmail.com>
2017-12-11 21:36:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,37 +1,19 @@
+
 var playlist = {
-  usher: 'confession',
-  linkinpark: 'in the end',
-  eminem: 'rap god'
+  artist1: "song1",
+  artist2: "song2",
+  artist3: "song3"
+};
+
+function updatePlaylist(thePlaylist, artistName, songTitle) {
+  return Object.assign({}, thePlaylist, { [artistName]: songTitle});
 }
 
-function updatePlaylist(thePlaylist, artistName, songTitle
-return object.assign({},thePlaylist, { [artist]: songTitle})
-}
+updatePlaylist(playlist, "artist4", "song4");
 
-updatedPlaylist(playlist, 'chainsmoker', 'dreaming'
-
-function removeFromPlaylist(thePlaylist, artist) {
-  delete thePlaylist.artist
+function removeFromPlaylist(thePlaylist, artistName) {
+  delete thePlaylist[artistName];
   return thePlaylist;
 }
 
-removeFromPlaylist(playlist, 'linkinpark');
-
-// var playlist = {
-//   artist1: "song1",
-//   artist2: "song2",
-//   artist3: "song3"
-// };
-
-// function updatePlaylist(thePlaylist, artistName, songTitle) {
-//   return Object.assign({}, thePlaylist, { [artistName]: songTitle});
-// }
-
-// updatePlaylist(playlist, "artist4", "song4");
-
-// function removeFromPlaylist(thePlaylist, artistName) {
-//   delete thePlaylist[artistName];
-//   return thePlaylist;
-// }
-
-// removeFromPlaylist(playlist, "artist2");
+removeFromPlaylist(playlist, "artist2");
