2c390f4fbafd44311bb4ea46598828d94c7a0ec1
Ronald Li <ronaldbrooklynli@gmail.com>
2017-12-11 21:31:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,30 +1,30 @@
-// var playlist = {
-//   usher: 'confession',
-//   linkinpark: 'in the end',
-//   eminem: 'rap god'
-// }
-
-// function updatePlaylist(thePlaylist, artist, song) {
-// return object.assign({},thePlaylist{[artist]:song})
-// }
-
-// updatedPlaylist(playlist, 'chainsmoker', 'dreaming'
-
 var playlist = {
-  artist1: "song1",
-  artist2: "song2",
-  artist3: "song3"
-};
-
-function updatePlaylist(thePlaylist, artistName, songTitle) {
-  return Object.assign({}, thePlaylist, { [artistName]: songTitle});
+  usher: 'confession',
+  linkinpark: 'in the end',
+  eminem: 'rap god'
 }
 
-updatePlaylist(playlist, "artist4", "song4");
-
-function removeFromPlaylist(thePlaylist, artistName) {
-  delete thePlaylist[artistName];
-  return thePlaylist;
+function updatePlaylist(thePlaylist, artist, song) {
+return object.assign({},thePlaylist{[artist]:song})
 }
 
-removeFromPlaylist(playlist, "artist2");
+updatedPlaylist(playlist, 'chainsmoker', 'dreaming'
+
+// var playlist = {
+//   artist1: "song1",
+//   artist2: "song2",
+//   artist3: "song3"
+// };
+
+// function updatePlaylist(thePlaylist, artistName, songTitle) {
+//   return Object.assign({}, thePlaylist, { [artistName]: songTitle});
+// }
+
+// updatePlaylist(playlist, "artist4", "song4");
+
+// function removeFromPlaylist(thePlaylist, artistName) {
+//   delete thePlaylist[artistName];
+//   return thePlaylist;
+// }
+
+// removeFromPlaylist(playlist, "artist2");
